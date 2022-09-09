8bfaf25ee05cd636a53f40d5788dc51c6a5ba254
lellabhaskar <lellabhaskar@gmail.com>
2022-09-09 05:56:01 +0000

    added covid,main and weather files

---
+++ covidApp.py
@@ -0,0 +1,41 @@
+import json
+import datetime
+import requests
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def covid():
+    if request.method == 'POST':
+        country=request.form["country"]
+
+        print(country)
+        url = "https://api.covid19api.com/summary"
+        print(url)
+
+                    #1 using requests module
+
+        resp=requests.get(url).json()
+        print(resp)
+
+        dictdata={}
+        #rescountrydata = {'covid':resp["Countries"]}
+        rescountrydata = resp["Countries"]
+        for countrydata in rescountrydata:
+            if countrydata['Country']==country:
+               dictdata={'covid':countrydata['Country'],'TotalCases':countrydata['TotalConfirmed'],
+                         'TotalDeaths':countrydata['TotalDeaths']}
+               break
+
+        return render_template('covidinfo.html', covidinfo=dictdata)
+    else:
+        dictdata =None
+        return render_template('covidinfo.html',covidinfo=dictdata)
+
+app.run()
+
+#heroku login
+#git init
+# git status
+#git add .
+#git commit -am "make it better"
